cfee0ca89c69837baa0222941418967806ab7f96
Dzianis Kundaliou <dark_dionis@mail.ru>
2023-02-22 14:48:30 +0300

    Homework was deleted from this repo

--- homework/longCalculations/test.js
+++
@@ -1,9 +0,0 @@
-const { testSum } = require('./longSumTest');
-const { testDiffrence } = require('./longDiffrenceTest');
-const { testProd } = require('./longProdTest');
-const { testDivision } = require('./longDivisionTest')
-
-testSum();
-testDiffrence();
-testProd();
-testDivision();
